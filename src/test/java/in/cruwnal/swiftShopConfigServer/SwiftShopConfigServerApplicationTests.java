509de52fce3d790b34e3c6e48050c3bbbfd84915
Kunal Nagwanshi <nagwanshikunal22@gmail.com>
2026-02-14 23:21:34 +0530

    Setup CI/CD and Logic Repo

---
+++ src/test/java/in/cruwnal/swiftShopConfigServer/SwiftShopConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.cruwnal.swiftShopConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftShopConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
